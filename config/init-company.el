3a824c6cfe0618dc111fcf21dd13f1bd2316255b
Ke Xu <kdotxu@gmail.com>
2017-01-26 13:31:20 -0500

    evil repeat company completion

--- config/init-company.el
+++ config/init-company.el
@@ -5,13 +5,21 @@
   (setq company-idle-delay 0.2)
   (setq company-selection-wrap-around t)
   (define-key company-active-map [tab] 'company-complete)
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort)
+  (mapc #'evil-declare-change-repeat
+        '(company-complete
+          company-complete-common
+          company-select-next
+          company-select-previous
+          company-complete-selection
+          ))
 
-  (use-package company-quickhelp
-    :defer 1
-    :config
-    (company-quickhelp-mode 1))
+  ;; (use-package company-quickhelp
+  ;;   :config
+  ;;   (setq company-quickhelp-delay 0.5)
+  ;;   (company-quickhelp-mode 1))
+  
   (use-package company-tern
     :defer 1
     :config
